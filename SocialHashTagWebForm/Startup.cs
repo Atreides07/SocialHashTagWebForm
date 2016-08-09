8ee312dd0735185e65a8fd9158937ee7b5d7869c
Anton Novikov <tonn.post@gmail.com>
2016-08-09 13:11:13 +0400

    Явная миграция базы для CodeFirst

--- SocialHashTagWebForm/Startup.cs
+++ SocialHashTagWebForm/Startup.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.Owin;
+﻿using System.Data.Entity.Migrations;
+using Microsoft.Owin;
 using Owin;
+using SocialHashTagWebForm.Core.Repository;
 
 [assembly: OwinStartupAttribute(typeof(SocialHashTagWebForm.Startup))]
 namespace SocialHashTagWebForm
 {
-    public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Мигрируем базу с нормальным запуском Seed из Configuration
+            // http://stackoverflow.com/a/17339310
+            var configuration = new DbMigrationsConfiguration<VideoHashTagDbContext>
+            {
+                ContextType = typeof(VideoHashTagDbContext),
+                AutomaticMigrationsEnabled = true
+            };
+
+            var migrator = new DbMigrator(configuration);
+
+            //This will run the migration update script and will run Seed() method
+            migrator.Update();
+            // -----------------------------------
+
             ConfigureAuth(app);
         }
     }
